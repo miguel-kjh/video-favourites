7ecd69bdceb966bf64f21e1c3a9466a9f2c6a547
miguel-kjh <miguelangel.kjh@gmail.com>
2021-07-17 15:31:06 +0100

    doc

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Getting Started with Create React App
 
+Example 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
           <List></List>
-        </p>
       </header>
     </div>
   );
